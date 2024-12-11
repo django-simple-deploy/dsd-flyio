b14ddd66cc175aa90a8252f3e909086922ac056e
Eric Matthes <ehmatthes@gmail.com>
2024-12-11 10:29:46 -0500

    Uses config instead of returning individual attributes to core.

--- dsd_flyio/__init__.py
+++ dsd_flyio/__init__.py
@@ -1,3 +1,4 @@
-from .deploy import simple_deploy_automate_all_supported
-from .deploy import simple_deploy_get_automate_all_msg
+# from .deploy import simple_deploy_automate_all_supported
+# from .deploy import simple_deploy_get_automate_all_msg
+from .deploy import simple_deploy_get_plugin_config
 from .deploy import simple_deploy_deploy
