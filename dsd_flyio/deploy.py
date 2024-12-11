b30d05d804b63800f44d613972d281828b5fb0e5
Eric Matthes <ehmatthes@gmail.com>
2024-12-11 10:31:52 -0500

    Remove diagnostic code.

--- dsd_flyio/deploy.py
+++ dsd_flyio/deploy.py
@@ -21,28 +21,6 @@ def simple_deploy_get_plugin_config():
     return plugin_config
 
 
-# @simple_deploy.hookimpl
-# def simple_deploy_automate_all_supported():
-#     """Specify whether --automate-all is supported on the specified platform."""
-#     return True
-
-
-# @simple_deploy.hookimpl
-# def simple_deploy_get_automate_all_msg():
-#     """Get platform-specific confirmation message for --automate-all flag."""
-#     return platform_msgs.confirm_automate_all
-
-
-# @simple_deploy.hookimpl
-# def simple_deploy_get_platform_name():
-#     """Return the name of the platform that's being deployed to.
-
-#     DEV: Consider returning a config object if there ends up being more info to share
-#     back to core.
-#     """
-#     return "fly_io"
-
-
 @simple_deploy.hookimpl
 def simple_deploy_deploy():
     """Carry out platform-specific deployment steps."""
